0e688add8bf7e5cedbf24b884398da3c15fc399f
Mark <mark@witsec.nl>
2018-11-12 20:23:44 +0100

    Added hide on publish, changed top/bottom padding and some other things

---
+++ CHANGELOG.md
@@ -0,0 +1,15 @@
+# Changelog
+
+All notable changes to this project will be documented in this file.
+
+## 0.2 (2018-11-12)
+
+- Added 'Hide Block on Publish' switch. This is useful if you're adding Javascript only, or when HTML shouldn't be visible
+- Added a check to reset padding to 2, when HTML is empty and padding is set to 0. Otherwise, the user would lose control of the block within Mobirise
+- Split 'Padding Top/Bottom' into 'Padding Top' and 'Padding Bottom'
+- Renamed extension from 'Custom HTML Block' to 'Custom HTML'
+- Added 'Learn more...' link in extension description
+
+## 0.1 (2018-01-01)
+
+This is the initial release of the Custom HTML extension for Mobirise.
--- src/witsec-custom-html-block/component.js
+++ src/witsec-custom-html-block/component.js
@@ -1,7 +1,7 @@
 mbrApp.loadComponents(
 	"witsec-custom-html",
 	{"witsec-custom-html-block":{
-			_group:"Article",
+			_group:"witsec",
 			_onParamsShow: function(e,$params,$block) {
 				// If we don't encode the textareas, it'll mess up the HTMLCode textarea in the gear box
 				// And we have to do this again in the _onParamsChange block as well
@@ -9,8 +9,10 @@ mbrApp.loadComponents(
 				this._params.HTMLCode = this.HTMLCode.replace(/<\/textarea/gim, "&lt;/textarea")
 			},
 			_params:{
-				HTMLCode:{type:"textarea",title:"HTML:",default:"Enter your HTML here"},
-				paddingTop:{type:"range",title:"Top/Bottom",min:0,max:8,step:1,default:2},
+				HTMLCode:{type:"textarea",title:"HTML:",default:"Your HTML goes here. Please use the gear-icon to edit the HTML."},
+				hideOnPublish:{type:"switch",title:"Hide Block on Publish",default:!1},
+				paddingTop:{type:"range",title:"Top Padding",min:0,max:8,step:1,default:2,halfWidth:true},	
+				paddingBottom:{type:"range",title:"Bottom Padding",min:0,max:8,step:1,default:2,halfWidth:true},
 				bgImageRadio:{type:"radio",title:"Background Image",name:"bgType",default:!1},
 				bgImage:{type:"image",title:"",default:"images/bg3.jpg",condition:["bgImageRadio"]},
 				parallax:{type:"switch",title:"Parallax",default:!0,condition:["bgImageRadio"]},
@@ -21,16 +23,38 @@ mbrApp.loadComponents(
 				overlayOpacity:{type:"range",title:"Opacity",min:0,max:1,step:.1,default:.5,condition:["overlay","!bgColorRadio"]}
 			},
 			HTMLCode:"Your HTML goes here. Please use the gear-icon to edit the HTML.",
-			paddingTop:".block {padding-bottom:2rem; padding-top:2rem;}",
-			_onParamsChange:function(c,a,b){
-				"HTMLCode"==a&&(
-					this.HTMLCode=b,
-					this._params.HTMLCode=this.HTMLCode.replace(/<\/textarea/gim, "&lt;/textarea")
-				);
-				"paddingTop"==a&&(
-					this.paddingTop=".block {padding-bottom:"+b+"rem; padding-top:"+b+"rem;}"
-				)
-			}	
+			_onParamsChange:function($item, param, val){
+				if (param == "HTMLCode") {
+					this.HTMLCode = val;
+					this._params.HTMLCode = this.HTMLCode.replace(/<\/textarea/gim, "&lt;/textarea");
+				}
+
+				if (param == 'paddingTop') {
+					$item.find('.witsec-custom-html-container').css('padding-top', val + 'rem');
+				}
+
+				if (param == 'paddingBottom') {
+					$item.find('.witsec-custom-html-container').css('padding-bottom', val + 'rem');
+				}
+
+				if (typeof paramName === 'undefined') {
+					$item.find('.witsec-custom-html-container').css({'padding-top': this._params.paddingTop + 'rem','padding-bottom': this._params.paddingBottom + 'rem'});
+				}
+
+				// If the HTML is empty and all padding set to 0, you kinda lose the block. Let's assist the user and give him back control
+				if (this._params.HTMLCode == "" && this._params.paddingTop == 0 && this._params.paddingBottom == 0) {
+					this._params.paddingTop = 2;
+					this._params.paddingBottom = 2;
+					$item.find('.witsec-custom-html-container').css({'padding-top': this._params.paddingTop + 'rem','padding-bottom': this._params.paddingBottom + 'rem'});
+					alert("WARNING: your HTML is empty and padding is set to 0. This makes your block very hard to reach within Mobirise. I've reset padding to 2, so the block is properly visible.");
+				}
+			},
+			_publishFilter: function($obj) {
+				if (this._params.hideOnPublish)
+					$obj.find('.witsec-custom-html-container').css({'padding-top': '0rem','padding-bottom': '0rem'});
+				else
+					$obj.find('.witsec-custom-html-container').css({'padding-top': this._params.paddingTop + 'rem','padding-bottom': this._params.paddingBottom + 'rem'});
+			}
 		}
 	}
 );
